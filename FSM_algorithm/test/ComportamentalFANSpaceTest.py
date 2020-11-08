09a854dfe5852998d0b1a9effdb748d041faaf6f
stoppini <rove.albe@gmail.com>
2020-11-08 22:25:08 +0100

    finiti test su FanSpace

--- FSM_algorithm/test/ComportamentalFANSpaceTest.py
+++ FSM_algorithm/test/ComportamentalFANSpaceTest.py
@@ -1,6 +1,7 @@
 import unittest
 from userInputOutput import UserInputOutput as UserIO
 from ComportamentalFANSpace import ComportamentalFANSpace
+from core.State import State
 
 
 class TestStringMethods(unittest.TestCase):
@@ -26,6 +27,27 @@ class TestStringMethods(unittest.TestCase):
         b = fanSpace._space_states[5]
         fanSpace._prune()
         self.assertTrue(a, b in fanSpace._space_states)
+
+    def test_add_state_invalid(self):
+        f = open('sample/FSCNetwork.sample.json', 'r')
+        lines = [line.strip() for line in f]
+        cfaNetwork = UserIO.readInput(''.join(line for line in lines))
+        fanSpace = ComportamentalFANSpace(cfaNetwork)
+        fanSpace._initialize()
+        fanSpace._add_states(fanSpace._space_states[0], fanSpace.
+                             _space_states[0].next_transition_state(), 1)
+        fanSpace._add_states(fanSpace._space_states[1], fanSpace.
+                             _space_states[1].next_transition_state(), 2)
+        fanSpace1 = ComportamentalFANSpace(cfaNetwork)
+        fanSpace1._initialize()
+        fanSpace1._add_states(fanSpace1._space_states[0], fanSpace1.
+                              _space_states[0].next_transition_state(), 1)
+        fanSpace1._add_states(fanSpace1._space_states[1], fanSpace1.
+                              _space_states[1].next_transition_state(), 2)
+        fanSpace1._add_states(fanSpace1._space_states[1], fanSpace1.
+                              _space_states[0].next_transition_state(), 2)
+        self.assertFalse(fanSpace._space_states[1] !=
+                         fanSpace1._space_states[1])
 
     def test_add_state_valid(self):
         f = open('sample/FSCNetwork.sample.json', 'r')
@@ -60,6 +82,75 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(fanSpace._space_states[0]._states[0] in
                         fanSpace._space_states[5]._states and
                         fs_dim_l_it + 1 == len(fanSpace._space_states))
+
+    def test_add_state_valid_expected_state(self):
+        out_exp_1 = {
+            'name': 't2a',
+            'destination': '21',
+            'link': [{
+                'type': 'in',
+                'link': 'L2',
+                'event': 'e2'},
+                {
+                'type': 'out',
+                'link': 'L3',
+                'event': 'e3'}],
+            'observable': 'o2',
+            'relevant': None}
+        out_exp_2 = {
+            'name': 't3b',
+            'destination': '30',
+            'link': [{
+                'type': 'in',
+                'link': 'L3',
+                'event': 'e3'}],
+            'observable': None,
+            'relevant': None}
+        out_exp_3 = {
+            'name': 't3c',
+            'destination': '31',
+            'link': [{
+                'type': 'in',
+                'link': 'L3',
+                'event': 'e3'}],
+            'observable': None,
+            'relevant': 'f'}
+        state_exp_1 = State('20', True, [out_exp_1])
+        state_exp_2 = State('31', False, [out_exp_2, out_exp_3])
+        f = open('sample/FSCNetwork.sample.json', 'r')
+        lines = [line.strip() for line in f]
+        cfaNetwork = UserIO.readInput(''.join(line for line in lines))
+        fanSpace = ComportamentalFANSpace(cfaNetwork)
+        fanSpace._initialize()
+        fanSpace._add_states(fanSpace._space_states[0], fanSpace.
+                             _space_states[0].next_transition_state(), 1)
+        self.assertTrue(state_exp_1, state_exp_2 in
+                        fanSpace._space_states[1]._states and
+                        len(fanSpace._space_states[1]._states) == 2)
+
+    def test_add_state_invalid_expected_state(self):
+        out_exp_1 = {
+            'name': 't2c',
+            'destination': '21',
+            'link': [{
+                'type': 'in',
+                'link': 'L2',
+                'event': 'e2'},
+                {
+                'type': 'out',
+                'link': 'L3',
+                'event': 'e3'}],
+            'observable': 'o2',
+            'relevant': None}
+        state_exp_1 = State('20', True, [out_exp_1])
+        f = open('sample/FSCNetwork.sample.json', 'r')
+        lines = [line.strip() for line in f]
+        cfaNetwork = UserIO.readInput(''.join(line for line in lines))
+        fanSpace = ComportamentalFANSpace(cfaNetwork)
+        fanSpace._initialize()
+        fanSpace._add_states(fanSpace._space_states[0], fanSpace.
+                             _space_states[0].next_transition_state(), 1)
+        self.assertFalse(state_exp_1 in fanSpace._space_states[1]._states)
 
 
 if __name__ == '__main__':
